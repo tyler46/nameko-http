a8af7e3b402b22a8bcba65b6f494123a59365d0b
spyros <markopoulos@vermantia.com>
2018-11-01 18:31:51 +0200

    Bump version: 0.1.0 → 0.1.1

--- nameko_http/__init__.py
+++ nameko_http/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Spyros Markopoulos"""
 __email__ = 'virtualssuport@vermantia.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 from .http_api import api
--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
         'requests-mock>=1.5.2',
     ],
     url='https://app.assembla.com/spaces/retail-platform/git-12/source',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
