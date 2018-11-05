a313d549a644a3f5883e207112858326d0bbc470
spyros <markopoulos@vermantia.com>
2018-11-05 12:05:01 +0200

    Fix incorrect bumping

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
         'requests-mock>=1.5.2',
     ],
     url='https://app.assembla.com/spaces/retail-platform/git-12/source',
-    version='version='version='0.1.2''',
+    version='0.1.2',
     zip_safe=False,
 )
